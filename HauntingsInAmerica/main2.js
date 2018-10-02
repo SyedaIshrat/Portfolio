c8accf9c0f6d218a8b0aad0efbf7043a23ed2296
SyedaIshrat <Rasyedais@gmail.com>
2018-10-02 03:38:40 -0400

    Add files via upload

---
+++ HauntingsInAmerica/main2.js
@@ -0,0 +1,28 @@
+$(function(){
+				$('#maximage').maximage({
+					cycleOptions: {
+						fx: 'fade',
+						speed: 1000,
+						timeout: 5000,
+						prev: '#arrow_left',
+						next: '#arrow_right',
+						pause: 0,
+						before: function(last,current){
+							if(!$.browser.msie){
+								if($(current).find('video').length > 0) $(current).find('video')[0].play();
+							}
+						},
+						after: function(last,current){
+							if(!$.browser.msie){
+								if($(last).find('video').length > 0) $(last).find('video')[0].pause();
+							}
+						}
+					},
+					onFirstImageLoaded: function(){
+						jQuery('#cycle-loader').hide();
+						jQuery('#maximage').fadeIn('fast');
+					}
+				});
+				jQuery('video,object').maximage('maxcover');
+	
+			});
